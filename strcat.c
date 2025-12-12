49b183cf30e48709ed0f101492dc8d4538d9520a
Mist52 <chendc212@gmail.com>
2025-12-12 14:33:06 -0500

    testing atoi

--- strcat.c
+++ strcat.c
@@ -23,6 +23,16 @@ char *t;
     return ret;
 }
 
+int atoi(char s[]) {
+    int i, n;
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
+}
+
+
+
 main()
 {
     char dest[40];          
