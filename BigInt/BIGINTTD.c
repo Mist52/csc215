df7e88c19d9893685e494bb882aa04735006059c
Mist52 <chendc212@gmail.com>
2026-01-08 10:43:42 -0500

    started the adding

--- BigInt/BIGINTTD.c
+++ BigInt/BIGINTTD.c
@@ -9,6 +9,15 @@ main() {
         set_bigint("1234567", &bi);
         ASSERT_STR(get_bigint(bi), "1234567");
     }
-
+TEST_CASE("Adding bigints") {
+    struct BigInt bi1;
+    struct BigInt bi2;
+    struct BigInt biout;
+    char buffer3[256];
+    char buffer4[256];
+    char buffer5[256];
+    char buffer6[256];
+    char* temp2;
+}
     END_TESTING();
 }
